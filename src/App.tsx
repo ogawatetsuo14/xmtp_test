1f991c725f2717badac4080bf0956ed51152004c
Tetsuo Ogawa <ogawatetsuo14@gmail.com>
2024-06-20 01:41:15 +0900

    change for v2

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import LoginView from "./views/LoginView";
 import { useClient } from "./hooks/useClient";
 // import HomeView from "./views/HomeView";
 import NewConversationView from "./views/NewConversationView";
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en.json";
-
-TimeAgo.addDefaultLocale(en);
 
 function App() {
   const client = useClient();
